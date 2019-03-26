fa6f82c7cb926766395de395fe99477ac5295777
Tim Kreutz <tjkreutz@gmail.com>
2019-03-26 10:46:32 +0100

    Change blank config file to environment variables for deployment

--- src/config.py
+++ src/config.py
@@ -1,5 +1,7 @@
-HOST = ""
-USER = ""
-PASSWORD = ""
-DB = ""
-PORT = 0
+import os
+
+HOST = os.environ.get('HOST')
+USER = os.environ.get('USER')
+PASSWORD = os.environ.get('PASS')
+DB = os.environ.get('DB')
+PORT = int(os.environ.get('PORT'))
