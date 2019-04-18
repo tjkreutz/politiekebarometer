c6a29189042bc465bf95bfe734d97d123ab10375
Tim Kreutz <tjkreutz@gmail.com>
2019-04-18 13:36:07 +0200

    A start to dummy overview-dossiers page

--- pages/overview_dossiers.py
+++ pages/overview_dossiers.py
@@ -1,27 +1,38 @@
+import dash_core_components as dcc
 import dash_html_components as html
 
 def get_layout():
     layout = html.Div([
+        html.Div(html.H2('Dossiers'), className='title-field'),
         html.Div([html.P("""Dummy intro.""")]
         , className='intro-text'),
         html.Hr(),
-        html.Div(html.H2('Dossiers'), className='title-field'),
-        html.Div([
+        html.Center([
+            html.B("Figuur 1: "),
+            html.Span("Welke dossiers werden de afgelopen 7 dagen vaakst genoemd op Twitter en in het online nieuws?")
+        ],className='description'),
+        dcc.Link(
             html.Div([
-                html.Div('dossier1', className='dossier')
-            ], className='six columns'),
-            html.Div([
-                html.Div('dossier2', className='dossier')
-            ], className='six columns')
-        ], className='row'),
-        html.Div([
-            html.Div([
-                html.Div('dossier3', className='dossier')
-            ], className='six columns'),
-            html.Div([
-                html.Div('dossier4', className='dossier')
-            ], className='six columns')
-        ], className='row')
+                html.Div([
+                    html.H2('1. Brexit (700)')
+                ], className='top-bar'),
+                html.Div([
+                    html.Div([
+                        html.Div([
+                            html.Img(src='/assets/brexit.jpg'),
+                            html.P("""
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel imperdiet metus, cursus 
+                            condimentum orci. Mauris aliquet metus elit... 
+                            """),
+                        ], className='eight columns'),
+                        html.P("""
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel imperdiet metus, cursus 
+                        condimentum orci.
+                        """, className='four columns'),
+                    ], className='row')
+                ], className='bottom-content'),
+            ], className='dossier')
+        , href='#'),
     ])
 
     return layout
